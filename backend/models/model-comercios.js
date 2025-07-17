66c6ee90d2f42549de449bd7883fc6777918a70c
valenvii <valentina.villarroel@est.fi.uncoma.edu.ar>
2025-07-17 19:30:52 -0300

    Agrego booleanos a comercio y descuentos para ver si estan o no activos. Avanzo en los modelos de comercios y descuentos

--- backend/models/model-comercios.js
+++ backend/models/model-comercios.js
@@ -6,11 +6,17 @@ const fileComercios = './backend/json/comercios.json';
 const obtenerJson = () => {
     //Leemos el archivo con comercios
     const archivoComercios = fs.readFileSync(fileComercios, 'utf-8');
-
-    //Lo transformamos a JSON
     const comerciosJSON = JSON.parse(archivoComercios);
 
     return comerciosJSON;
 };
+
+const escribirJson = (comerciosJSON) => {
+    const jsonData = JSON.stringify(comerciosJSON);
+
+    //Escribimos el archivo con comercios actualizado
+    fs.writeFileSync(archivoComercios, jsonData, 'utf-8');
+};
+
 
 module.exports = {obtenerJson};
--- backend/models/model-descuentos.js
+++ backend/models/model-descuentos.js
@@ -6,11 +6,115 @@ const fileDescuentos = './backend/json/descuentos.json';
 const obtenerJson = () => {
     //Leemos el archivo con descuentos
     const archivoDescuentos = fs.readFileSync(fileDescuentos, 'utf-8');
-
-    //Lo transformamos a JSON
     const descuentosJSON = JSON.parse(archivoDescuentos);
 
     return descuentosJSON;
 };
 
-module.exports = {obtenerJson};
+const escribirJson = (descuentosJSON) => {
+    const jsonData = JSON.stringify(descuentosJSON);
+    //Escribimos el archivo con comercios actualizado
+    fs.writeFileSync(archivoDescuentos, jsonData, 'utf-8');
+};
+
+const obtenerDescuentosComercio = (nombreComercio) => {
+    //Obtiene los descuentos para un nombre de comercio
+
+    const descuentosJSON = obtenerJson();
+    //buscamos el tipo de patron que tenga el mismo nombre que el pasado por parámetro
+    const descuentos = descuentosJSON.find(descuento => descuento.comercio == nombreComercio);
+
+    return descuentos;
+};
+
+const obtenerDescuento = (nombreComercio, idDescuento) => {
+    //Obtiene un descuento de un comercio
+
+    let descuento = null;
+    //Obtenemos los descuentos para ese nombre de comercio
+    const descuentos = obtenerDescuentosComercio(nombreComercio);
+
+    if(descuentos) {
+        //Si existen descuentos para ese comercio 
+        descuento = (descs.descuentos).find(desc => desc.id == idDescuento);
+    }
+
+    return descuento;
+};
+
+/*
+Algo asi deberia ser para obtener todos los descuentos, pero calculo que esto te devuelve una lista de listas. Tipo una lista en donde cada elem es 
+la lista de descuentos de cada comercio. Para desp usarla para paginacion. 
+
+const obtenerDescuentos = () => {
+    //Obtiene todos los descuentos
+
+    let listaDescuentos = null;
+    const descuentosJSON = obtenerJson();
+    listaDescuentos = descuentos.map(function (descuento) {return descuento.descuentos});
+
+    return listaDescuentos;
+};
+
+const obtenerListaDescuentos = (cantidad, inicio) => {
+    let listaDescuentos = null
+    const descuentos = obtenerJson();
+    
+};
+
+*/
+
+const existenDescuentos = (nombreComercio) => {
+    //Devuelve verdadero si ese comercio tiene descuentos
+    let existen = false;
+    const descuentos = obtenerDescuentosComercio(nombreComercio);
+
+    if(descuentos) {
+        existen = true;
+    }
+
+    return existen;
+};
+
+const existeDescuento = (nombreComercio, idDescuento) => {
+    //Devuelve verdadero si ese comercio contiene a ese descuento
+    let existe = false;
+    const descuento = obtenerDescuento(nombreComercio, idDescuento);
+
+    if(descuento) {
+        existe = true;
+    }
+
+    return existe;
+};
+
+const agregarDescuento = (nuevoDescuento, nombreComercio) => {
+    //Agrega un descuento a un comercio
+
+    const descuentosJSON = obtenerJson();
+    //buscamos el tipo de patron que tenga el mismo nombre que el pasado por parámetro
+    const listaDescuentos = descuentosJSON.find(descuento => descuento.comercio == nombreComercio);
+
+    if(listaDescuentos) {
+        (listaDescuentos.descuentos).push(nuevoDescuento);
+        escribirJson(descuentosJSON);
+    }
+};
+
+const eliminarDescuento = (nombreComercio, idDescuento) => {
+    //Invalida un descuento de un comercio
+
+    const descuentosJSON = obtenerJson();
+    const listaDescuentos = descuentosJSON.find(lisDescuentos => lisDescuentos.nombre == nombreComercio);
+
+    if(listaDescuentos) {
+        const descuento = (listaDescuentos.descuentos).find(desc => desc.id == idDescuento);
+
+        if(descuento) {
+            listaDescuentos.descuentos = (listaDescuentos.descuentos).filter(desc => desc.id !== idDescuento);
+            escribirJson(descuentosJSON);
+        }
+    }
+}; 
+
+module.exports = {obtenerJson, escribirJson, obtenerDescuento, obtenerDescuentosComercio, existenDescuentos, existeDescuento, agregarDescuento, eliminarDescuento};
